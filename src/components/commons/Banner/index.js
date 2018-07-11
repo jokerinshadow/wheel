c8f4f2529b5fc9fcf12facee5f8605610153e35f
koudiddy <215594250@qq.com>
2018-07-11 17:37:47 +0800

    commonheader03

--- src/components/commons/Banner/index.js
+++ src/components/commons/Banner/index.js
@@ -1,10 +1,17 @@
 import React,{Component} from 'react'
+import Swiper from 'swiper'
 import './index.scss'
 
 class Banner extends Component{
     render(){
         return (
-            <div className='banner'></div>
+            <div className='banner swiper-container'>
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">Slide 1</div>
+                    <div class="swiper-slide">Slide 2</div>
+                    <div class="swiper-slide">Slide 3</div>
+                </div>
+            </div>
         )
     }
 }
